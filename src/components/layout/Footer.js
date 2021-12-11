42fa324f98c012d9c30418672729d91fdf9b428a
Cristian Poley <cristianpoleygonzalez@gmail.com>
2021-12-11 19:47:54 +0100

    fix: error href in footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
             <nav>
                 <a href="https://github.com/cristianpoleyJS" target="_blank" without rel="noreferrer" aria-label="GitHub"><IconGithub width={30} height={30} /></a>
                 <a href="https://twitter.com/cristianpoley" target="_blank" without rel="noreferrer" aria-label="Twitter"><IconTwitter width={30} height={30} /></a>
-                <a href="https://linkedin.com/cristian-poley" target="_blank" without rel="noreferrer" aria-label="YouTube"><IconLinkedin width={30} height={30} /></a>
+                <a href="https://linkedin.com/in/cristian-poley/" target="_blank" without rel="noreferrer" aria-label="YouTube"><IconLinkedin width={30} height={30} /></a>
             </nav>
         </footer>
     )
